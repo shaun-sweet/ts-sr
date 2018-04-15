b6b697b975c6f76a7e18c68e4afc8ff507cd87eb
Shaun Sweet <shaun@shaunsweet.com>
2018-04-14 20:28:25 -0700

    ci: fix ci

--- tools/getConfig.js
+++ tools/getConfig.js
@@ -2,19 +2,19 @@ const axios = require('axios')
 const fs = require('fs')
 
 const {
-  SR_CONFIG_KEY,
+  CONFIG_KEY,
   SR_CONFIG_CONTEXT
 } = process.env
 const CONFIG_API_URL = process.argv[2] || 'https://config.shaunsweet.com/rest/rawFile'
 const CONFIG_SAVE_TARGET = 'config.js'
 
 if (!CONFIG_KEY) throw new Error('Need ENV variable $SR_CONFIG_KEY exported in bash profile')
-if (!SR_CONFIG_CONTEXT) throw new Error('Need ENV variable $SR_CONFIG_CONTEXT exported in bash profile')
+if (!CONFIG_CONTEXT) throw new Error('Need ENV variable $SR_CONFIG_CONTEXT exported in bash profile')
 
 const client = axios.create({
   headers: {
-    'Client-Token': SR_CONFIG_KEY,
-    'Context': SR_CONFIG_CONTEXT,
+    'Client-Token': CONFIG_KEY,
+    'Context': CONFIG_CONTEXT,
     'File': 'config.js',
     'Application-name': 'ts-sr'
   }
@@ -25,6 +25,6 @@ client.get(CONFIG_API_URL)
   .then((res) => {
     fs.writeFile(CONFIG_SAVE_TARGET, res.data, (err) => {
       if (err) throw err
-      console.log(`"${SR_CONFIG_CONTEXT}" config details written to file in the project root -> .env`)
+      console.log(`"${CONFIG_CONTEXT}" config details written to file in the project root -> .env`)
     })
   })
